c5d87418634a6a400c53f0de6983f8c348890874
Hackbright Student <info@hackbrightacademy.com>
2015-04-09 12:42:35 -0700

    Added quit functionality, by adding an if conditional. Moved variable bindings outside of function.

--- calculator.py
+++ calculator.py
@@ -10,6 +10,8 @@ from arithmetic import *
 
 # Your code goes here
 
+
+
 x = True
 
 while x == True:
@@ -19,15 +21,20 @@ while x == True:
     tokens = input.split(" ")  
     tokens_len = len(tokens)
 
-    def calculator():
+    operator = tokens[0] 
 
-        operator = tokens[0] 
-
+    if tokens_len >= 2:
         num1 = float(tokens[1])
 
-        if tokens_len == 3:
-            num2 = float(tokens[2])
+    if tokens_len == 3:
+        num2 = float(tokens[2])
 
+    if operator == "quit":  
+        x = False
+        print "Ok, bai!"
+
+    def calculator():               
+        
         if operator == "pow":
             print power(num1,num2)
 
